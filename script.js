69bbf080d9dfcf9246d4136f6c441bf31793a14e
FKrizekk <125741066+FKrizekk@users.noreply.github.com>
2024-10-11 15:25:49 +0200

    some style updates + added saving the final-board

--- script.js
+++ script.js
@@ -6,3 +6,21 @@ function nameFunc(){
 function goToUrl(url) {
     window.location.href = url;
 }
+
+function saveElementAsJPG(elementId, fileName = 'screenshot.jpg') {
+    const element = document.getElementById(elementId);
+    
+    html2canvas(element, {
+        useCORS: true, // Allow cross-origin images
+        backgroundColor: null, // Capture the background color
+        allowTaint: true, // Allow tainted images
+        scale: 2, // Optional: Scale for higher resolution
+    }).then(canvas => {
+        const link = document.createElement('a');
+        link.href = canvas.toDataURL('image/jpeg');
+        link.download = fileName;
+        link.click();
+    }).catch(err => {
+        console.error("Error capturing the element:", err);
+    });
+}
